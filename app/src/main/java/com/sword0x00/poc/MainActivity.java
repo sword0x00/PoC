009df045ecacdf6925d3dc93bc7d297145469722
sword0x00 <mohanadhesham556@gmail.com>
2024-08-30 16:42:53 +0300

    initial commit of my first mobile (android) project

---
+++ app/src/main/java/com/sword0x00/poc/MainActivity.java
@@ -0,0 +1,45 @@
+package com.sword0x00.poc;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        TextView homeText = findViewById( R.id.home_text );
+        homeText.setText("lol fromsword!");
+        Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                homeText.setText(String.format("Clicked: %d",counter));
+                if (counter==10){
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://x.com"));
+                    startActivity(browserIntent);
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/sword0x00/poc/SecretActivity.java
@@ -0,0 +1,33 @@
+package com.sword0x00.poc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecretActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_secret);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Intent receviedIntent = getIntent();
+        String sharedText =receviedIntent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText!=null){
+            TextView debug_text =findViewById(R.id.debug_text);
+            debug_text.setText("Shared: "+sharedText);
+        }
+
+    }
+}
